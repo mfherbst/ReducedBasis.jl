8220b2aec3c755b93c62381a5cde4e3cb50b36e5
Documenter.jl <documenter@juliadocs.github.io>
2022-11-25 16:28:59 +0000

    build based on 5a7cac5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ReducedBasis","category":"page"},{"location":"#ReducedBasis","page":"Home","title":"ReducedBasis","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ReducedBasis.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ReducedBasis]","category":"page"}]
+}
