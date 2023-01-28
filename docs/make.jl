6fcd76b04233827d019c55a5d297251bc5437c93
Michael F. Herbst <info@michael-herbst.com>
2023-01-28 20:37:47 +0100

    literate

--- docs/make.jl
+++ docs/make.jl
@@ -1,33 +1,145 @@
+# To manually generate the docs:
+#     1. Run "julia make.jl"
+#
+# To add a new example to the docs:
+#     1. Add the *.jl file to /examples, along with assets you require (e.g. input files,
+#        images, plots etc.)
+#     2. Add the example to the PAGES variable below, the asset files to EXAMPLE_ASSETS.
+#
+# To add a new page to the docs (not an example):
+#     1. Add the file to /docs/src, along with all assets. It can be a *.jl to be processed
+#        with Literate.jl and a *.md to be included as is.
+#     2. Add the file to the PAGES variable below. You don't need to track the assets.
+
+# Structure of the docs. List files as *.jl or *.md here. All files
+# ending in *.jl will be processed to *.md with Literate.
+PAGES = [
+    "Home" => "index.md",
+    "Examples" => [
+        "examples/xxz_ed.jl",
+        "examples/xxz_dmrg.jl",
+        "examples/xxz_pod.jl",
+        "examples/multi_ad.jl",
+    ],
+    "api.md",
+]
+
+# Files from the /examples folder that need to be copied over to the docs
+# (typically images, input or data files etc.)
+EXAMPLE_ASSETS = String[]  # "examples/abc.data"
+
+#
+# Configuration and setup
+#
+DEBUG = false  # Set to true to disable some checks and cleanup
+
+SRCPATH   = joinpath(@__DIR__, "src")
+BUILDPATH = joinpath(@__DIR__, "build")
+ROOTPATH  = joinpath(@__DIR__, "..")
+CONTINUOUS_INTEGRATION = get(ENV, "CI", nothing) == "true"
+BRANCH = try LibGit2.branch(LibGit2.GitRepo(ROOTPATH)) catch end
+
 push!(LOAD_PATH, "../src/")
+import LibGit2
 using ReducedBasis
 using Documenter
+using Literate
+
+#
+# Generate the docs
+#
+
+# Get list of files from PAGES
+extract_paths(pages::AbstractArray) = collect(Iterators.flatten(extract_paths.(pages)))
+extract_paths(file::AbstractString) = [file]
+extract_paths(pair::Pair) = extract_paths(pair.second)
+
+# Transform files to *.md
+transform_to_md(pages::AbstractArray) = transform_to_md.(pages)
+transform_to_md(file::AbstractString) = first(splitext(file)) * ".md"
+transform_to_md(pair::Pair) = (pair.first => transform_to_md(pair.second))
+
+# Copy assets over
+mkpath(joinpath(SRCPATH, "examples"))
+for asset in EXAMPLE_ASSETS
+    cp(joinpath(ROOTPATH, asset), joinpath(SRCPATH, asset), force=true)
+end
+
+# Collect files to treat with Literate (i.e. the examples and the .jl files in the docs)
+# The examples go to docs/literate_build/examples, the .jl files stay where they are
+literate_files = map(filter!(endswith(".jl"), extract_paths(PAGES))) do file
+    if startswith(file, "examples/")
+        (src=joinpath(ROOTPATH, file), dest=joinpath(SRCPATH, "examples"), example=true)
+    else
+        (src=joinpath(SRCPATH, file), dest=joinpath(SRCPATH, dirname(file)), example=false)
+    end
+end
+
+# Function to insert badges to examples
+function add_badges(str)
+    badges = [
+        "[![](https://mybinder.org/badge_logo.svg)](@__BINDER_ROOT_URL__/examples/@__NAME__.ipynb)",
+        "[![](https://img.shields.io/badge/show-nbviewer-579ACA.svg)](@__NBVIEWER_ROOT_URL__/examples/@__NAME__.ipynb)",
+    ]
+
+    # Find the Header and insert the badges right below
+    splitted = split(str, "\n")
+    idx = findfirst(startswith.(splitted, "# # "))
+    idx === nothing && error("Example files must start with # #")
+    insert!(splitted, idx + 1, "#md # " * badges[1])
+    insert!(splitted, idx + 2, "#md # " * badges[2])
+    insert!(splitted, idx + 3, "#md #")
+    join(splitted, "\n")
+end
+
+# Run Literate on them all
+for file in literate_files
+    preprocess = file.example ? add_badges : identity
+    Literate.markdown(file.src, file.dest;
+                      flavor=Literate.DocumenterFlavor(),
+                      credit=false, preprocess)
+    Literate.notebook(file.src, file.dest; credit=false,
+                      execute=CONTINUOUS_INTEGRATION || DEBUG)
+end
 
 DocMeta.setdocmeta!(ReducedBasis, :DocTestSetup, :(using ReducedBasis); recursive=true)
 
 makedocs(;
     modules=[ReducedBasis],
-    authors="Michael F. Herbst and Paul Brehmer",
     repo="https://github.com/mfherbst/ReducedBasis.jl/blob/{commit}{path}#{line}",
-    sitename="ReducedBasis.jl",
     format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
+        prettyurls = CONTINUOUS_INTEGRATION,
         canonical="https://mfherbst.github.io/ReducedBasis.jl",
         edit_link="master",
         assets=String[],
     ),
-    pages=[
-        "Home" => "index.md",
-        "Examples" => [
-            "examples/xxz_ed.md",
-            "examples/xxz_dmrg.md",
-            "examples/xxz_pod.md",
-            "examples/multi_ad.md",
-        ],
-        "api.md"
-    ],
+    sitename="ReducedBasis.jl",
+    authors="Michael F. Herbst and Paul Brehmer",
+    pages=transform_to_md(PAGES),
+    checkdocs=:exports,
+    strict=true,
 )
+
+# Dump files for managing dependencies in binder
+if CONTINUOUS_INTEGRATION && BRANCH == "master"
+    cp(joinpath(@__DIR__, "Project.toml"), joinpath(BUILDPATH, "Project.toml"), force=true)
+end
 
 deploydocs(;
     repo="github.com/mfherbst/ReducedBasis.jl",
     devbranch="master",
 )
+
+# Remove generated example files
+if !DEBUG
+    for file in literate_files
+        base = splitext(basename(file.src))[1]
+        for ext in [".ipynb", ".md"]
+            rm(joinpath(file.dest, base * ext), force=true)
+        end
+    end
+end
+
+if !CONTINUOUS_INTEGRATION
+    println("\nDocs generated, try $(joinpath(BUILDPATH, "index.html"))")
+end
